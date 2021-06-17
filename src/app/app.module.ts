86cac83f17156b7468d149127a2ab572a89e54ee
megha199 <meghamec1999@gmail.com>
2021-06-17 12:04:41 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HighlightDirective } from './highlight.directive';
+import { FeatureModule } from './feature.module';
+
 
 @NgModule({
-  imports:      [ BrowserModule],
-  declarations: [ AppComponent, HighlightDirective ],
+  imports:      [ BrowserModule,FeatureModule],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
